ed5e981b65a3436333912f8eb59091857cb79fbe
yono-oko <griffin.jame@husky.neu.edu>
2021-02-12 19:14:27 -0500

    edited start and service scripts

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ "x$PROD" == "x" ]]; then 
-	echo "This script is for starting in production."
-	echo "Use"
-	echo "   mix phx.server"
-	exit
-fi
+#if [[ "x$PROD" == "x" ]]; then
+#	echo "This script is for starting in production."
+#	echo "Use"
+#	echo "   mix phx.server"
+#	exit
+#fi
 
-# TODO: Enable this script by removing the above.
+# TODO: DONE. Enable this script by removing the above.
 
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
@@ -23,4 +23,3 @@ _build/prod/rel/practice/bin/practice start
 
 # TODO: Add a systemd service file
 #       to start your app on system boot.
-
